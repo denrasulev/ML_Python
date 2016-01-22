845304a8d2cde4c0688daef2b71416f0a28e67f3
Denis Rasulev <ranklord@gmail.com>
2016-01-22 17:33:32 +0600

    building recommender

--- recom.py
+++ recom.py
@@ -2,4 +2,11 @@
 
 import graphlab as gl
 
-song = gl.SFrame.read_csv('/Volumes/data/python/coursera/data/song_data.csv')
+song_data = gl.SFrame.read_csv('/Volumes/data/python/coursera/data/song_data.csv')
+
+print(song_data.head(n=10))
+
+gl.canvas.set_target('browser')
+
+song_data['song'].show()
+
